b20b2614fc14ef7bcdabe37e2358956f32dfba02
kolider <kolider@i.ua>
2021-10-01 17:36:54 +0300

    Update and rename statup.sh to startup.sh

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/#g' /etc/ssh/sshd_config
+#service ssh restart
+#apt update && apt install -y virtualbox-guest-utils virtualbox-guest-additions
+#mount -t vboxsf -o uid=1000,gid=1000,_netdev home_vagrant_ansible /home/vagrant/ansible #not recommended
